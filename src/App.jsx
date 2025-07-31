fbe0ac73f9279b14017a4cff531fa6a4f48e6da5
sIlENtbuffER <huangxiyan2311@gmail.com>
2025-07-31 10:59:07 -0700

    minimal fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import DualNBack from './modules/dualnback/DualNBack';
-import './components/Header.jsx'
-import './components/Footer.jsx'
+// import Footer from './components/Footer.jsx'
 import Header from './components/Header.jsx';
 
 function App() {
     return (
-        <div className='min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center'>
-            <Header/>
-            <h1 className = "text-3xl font-bold mb-4"> Brain Trainner</h1>
-            <DualNBack />
-        </div>
+        <div className="min-h-screen flex flex-col bg-gray-900 text-white">
+      <Header />
+
+      <main className="flex-1 flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold mb-4">Brain Trainner</h1>
+        <DualNBack />
+      </main>
+
+      {/* <Footer /> */}
+    </div>
     );
 }
 
